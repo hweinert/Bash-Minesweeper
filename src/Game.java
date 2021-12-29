d37551b9ca1ed623003670d796e2db2001a75fd0
Henry Weinert <weinerthenry@t-online.de>
2021-12-29 21:08:49 +0100

    High score lists get saved

--- src/Game.java
+++ src/Game.java
@@ -1,14 +1,22 @@
-import java.util.Scanner;
-
 import littlelib.StopWatch;
 import littlelib.Tools;
 import littlelib.score.HighScoreList;
 
 import java.io.File;
+import java.io.FileOutputStream; 
+import java.io.ObjectOutputStream;
+import java.io.FileInputStream; 
+import java.io.ObjectInputStream;
 import java.io.Serializable;
+import java.io.IOException;
+
+import java.util.Scanner;
+import java.util.List;
+import java.util.ArrayList;
 
 public class Game implements Serializable {
 	private static final long serialVersionUID = 6757400342605184116L;
+	private static final String SAVE_PATH = "Minesweeper.data";
 	private static Scanner scanner = new Scanner(System.in);
 	
 	private HighScoreList easyHighScoreList;
@@ -16,9 +24,14 @@ public class Game implements Serializable {
 	private HighScoreList hardHighScoreList;
 	
 	public Game() {
-		easyHighScoreList = new HighScoreList("Easy Mode", 2, "seconds", true);
-		normalHighScoreList = new HighScoreList("Normal Mode", 2, "seconds", true);
-		hardHighScoreList = new HighScoreList("Hard Mode", 2, "seconds", true);
+		File save = new File(SAVE_PATH);
+		if (save.exists()) {
+			load();
+		} else {
+			easyHighScoreList = new HighScoreList("Easy Mode", 2, "seconds", true);
+			normalHighScoreList = new HighScoreList("Normal Mode", 2, "seconds", true);
+			hardHighScoreList = new HighScoreList("Hard Mode", 2, "seconds", true);
+		}
 	}
 	
 	public void mainLoop() {
@@ -118,6 +131,7 @@ public class Game implements Serializable {
 			System.out.println("Congratulations! You reached a new high score!");
 			String name = Tools.saveStringInput("Enter your name: ", 3, 20);
 			rightList.add(name, watch.getSeconds());
+			save();
 		}
 	}
 	
@@ -125,5 +139,32 @@ public class Game implements Serializable {
 		System.out.println(easyHighScoreList);
 		System.out.println(normalHighScoreList);
 		System.out.println(hardHighScoreList);
+	}
+	
+	private void save() {
+		List<HighScoreList> scoreLists = new ArrayList<HighScoreList>();
+		scoreLists.add(easyHighScoreList);
+		scoreLists.add(normalHighScoreList);
+		scoreLists.add(hardHighScoreList);
+		
+		try (ObjectOutputStream oos = new ObjectOutputStream(new FileOutputStream(SAVE_PATH))) {
+			oos.writeObject(scoreLists);
+		} catch (IOException e) {
+			e.printStackTrace();
+		}
+	}
+	
+	private void load() {
+		try (ObjectInputStream ois = new ObjectInputStream(new FileInputStream(SAVE_PATH))) {
+			List<HighScoreList> scoreLists = new ArrayList<>();
+			scoreLists = (ArrayList<HighScoreList>) ois.readObject();
+			easyHighScoreList = scoreLists.get(0);
+			normalHighScoreList = scoreLists.get(1);
+			hardHighScoreList = scoreLists.get(2);
+		} catch (IOException e) {
+			e.printStackTrace();
+		} catch (ClassNotFoundException e) {
+			e.printStackTrace();
+		}
 	}
 }
